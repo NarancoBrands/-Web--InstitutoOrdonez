73817b976be4b0d3b5420a36af86c4e3499c2da3
IvanGarciaRubio <ivan.garcia@narancobrands.com>
2021-06-02 15:33:53 +0200

    cambios

--- src/app/components/admin/dashboard/calendar/calendar.component.ts
+++ src/app/components/admin/dashboard/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef, OnInit } from '@angular/core';
 import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours, parseISO } from 'date-fns';
-import { Subject } from 'rxjs';
+import { of, Subject } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView, DAYS_OF_WEEK, CalendarEventTitleFormatter } from 'angular-calendar';
 import { ToastrService } from 'ngx-toastr';
@@ -14,6 +14,8 @@ import { FlatpickrDefaultsInterface } from 'angularx-flatpickr/flatpickr-default
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { elementAt } from 'rxjs/operators';
+import { element } from 'protractor';
+import { el } from 'date-fns/locale';
 
 moment.updateLocale('en', {
   week: {
@@ -53,19 +55,17 @@ const colors: any = {
 export class CalendarComponent implements OnInit {
   @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
 
-  view: CalendarView = CalendarView.Month;
-
-  CalendarView = CalendarView;
-
-  viewDate: Date = new Date();
-
+  public view: CalendarView = CalendarView.Month;
+  public CalendarView = CalendarView;
+  public viewDate: Date = new Date();
+  public locale: string = "es";
   public agendas: Array<Agenda>;
-  public cliente: Cliente;
+  public clientes: Array<Cliente>;
   public agenda: Agenda;
-  public horario: Date = new Date();
+  public horario: any = [];
   public contadorHorario: number;
-
-  activeDayIsOpen: boolean = true;
+  public activeDayIsOpen: boolean = true;
+  public cliente:Array<any>;
 
   modalData: {
     action: string;
@@ -92,62 +92,9 @@ export class CalendarComponent implements OnInit {
     },*/
   ];
 
-  refresh: Subject<any> = new Subject();
+  //ARRAY DE EVENTOS EN EL CALENDARIO
 
-  constructor(private modal: NgbModal, private toastr: ToastrService, private _route: ActivatedRoute, private _router: Router,
-    private _agendaService: AgendaService, private _clienteservice: ClientesService, private fb: FormBuilder) {
-  }
-
-  //SUBSCRIBES DE LOS SERVICIOS DE AGENDAS Y CLIENTES
-
-  getAgendas() {
-    this.contadorHorario = 0;
-    this._agendaService.getClientesAgenda().subscribe(
-      result => {
-        this.agendas = result;
-        this.agendas.forEach(element => {
-          this.horario = element.prox_cita;
-          this.contadorHorario++;
-        });
-      },
-      error => {
-        console.log(<any>error);
-      }
-    );
-  }
-
-  getClientesPorId() {
-    let id
-    this._clienteservice.getClientesPorId(id).subscribe(
-      result => {
-        this.cliente = result;
-      },
-      error => {
-        console.log(<any>error);
-      }
-    );
-  }
-
-  getAgendasPorId() {
-    let id = 1;
-    this._agendaService.getClienteAgendaPorId(id).subscribe(
-      result => {
-        this.agenda = result;
-      },
-      error => {
-        console.log(<any>error);
-      }
-    );
-  }
-
-  ngOnInit(): void {
-    this.getAgendas();
-    this.getAgendasPorId();
-  }
-
-   //ARRAY DE EVENTOS EN EL CALENDARIO
-
-   events: CalendarEvent[] = [/*{
+  events: CalendarEvent[] = [/*{
     start: subDays(startOfDay(new Date()), 1),
     end: addDays(new Date(), 1),
     title: 'A 3 day event',
@@ -160,15 +107,125 @@ export class CalendarComponent implements OnInit {
     },
     draggable: true,
     
-  },*/
-    {
-      start: startOfDay(this.horario),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      actions: this.actions,
+  },*/];
 
-    },
-  ];
+  refresh: Subject<any> = new Subject();
+
+  constructor(private modal: NgbModal, private toastr: ToastrService, private _route: ActivatedRoute, private _router: Router,
+    private _agendaService: AgendaService, private _clienteservice: ClientesService, private fb: FormBuilder) {
+  }
+
+  //SUBSCRIBES DE LOS SERVICIOS DE AGENDAS Y CLIENTES
+
+  getAgendas() {
+    this._agendaService.getClientesAgenda().subscribe(
+      result => {
+        //guardamos los datos de las agendas
+        this.agendas = result;
+        //vamos a buscar los clientes
+        this.agendas.forEach(element=>{
+          this.horario=element.prox_cita;
+          this.getClientesPorId(this.agendas,this.horario,element.idCliente);
+        });
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  getClientePorIdModal(id){
+    //subscribe del cliente que necesitamos
+    this._clienteservice.getClientesPorId(id).subscribe(
+      result => {
+        //guardamos en una variable el resultado
+        this.cliente = result;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  //uso unico para el evento del principio
+  getClientesPorId(agendaCalendario,horarioCalendario,id) {
+    //guardamos por si acaso de nuevo las agendas con el dato que enviamos del metodo anterior
+    this.agendas=agendaCalendario;
+
+    //subscribe del cliente que necesitamos
+    this._clienteservice.getClientesPorId(id).subscribe(
+      result => {
+        //guardamos en una variable el resultado
+        this.cliente = result;
+        //metodo para añadir un evento
+        this.añadirEvento(horarioCalendario, this.cliente);
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  //sacar los datos de la agenda por un id concreto
+  getAgendaPorId(idModal){
+    let id=idModal;
+    this._agendaService.getClienteAgendaPorId(id).subscribe(
+      result => {
+        this.agenda=result;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  //AÑADIR EVENTO DIRECTAMENTE
+
+  añadirEvento(horario: Date, cliente): void {
+    //guardamos en dos variables los datos que nos llegan
+    let horarioEvento=horario;
+    var clienteEvento
+
+    //realizamos un for para guardar en una de esas variables los datos que necesitamos del modelo de cliente
+    for(let i=0; i<cliente.length; i++){
+      clienteEvento= [cliente[i].id,cliente[i].nombre, cliente[i].apellidos];
+    }
+    
+    //añadimos el evento
+    this.events = [
+      ...this.events,
+      {
+        title: clienteEvento,
+        start: startOfDay(new Date(horarioEvento)),
+        color: colors.red,
+        draggable: true,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true,
+        },
+      },
+    ];
+  }
+
+
+  //AÑADIR EVENTO DESDE EL MODAL
+
+  addEvent(): void {
+    this.events = [
+      ...this.events,
+      {
+        title: 'New event',
+        start: startOfDay(new Date()),
+        end: endOfDay(new Date()),
+        color: colors.red,
+        draggable: true,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true,
+        },
+      },
+    ];
+  }
 
   //EVENTO DE CLICKAR EN UN DIA
 
@@ -209,27 +266,11 @@ export class CalendarComponent implements OnInit {
   //ABRIR MODAL
 
   handleEvent(action: string, event: CalendarEvent): void {
+    let id=event.title[0];
     this.modalData = { event, action };
+    this.getAgendaPorId(id);
+    this.getClientePorIdModal(id);
     this.modal.open(this.modalContent, { size: 'xl' });
-  }
-
-  //AÑADIR EVENTO EN EL MODAL
-
-  addEvent(): void {
-    this.events = [
-      ...this.events,
-      {
-        title: 'New event',
-        start: startOfDay(new Date()),
-        end: endOfDay(new Date()),
-        color: colors.red,
-        draggable: true,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true,
-        },
-      },
-    ];
   }
 
   //BORRAR EVENTO
@@ -260,6 +301,11 @@ export class CalendarComponent implements OnInit {
     dateFormat: "Y-m-d H:i",
     // this:
     enable: [{ from: new Date(0, 1), to: new Date(new Date().getFullYear() + 200, 12) }]
+  }
+
+  ngOnInit(): void {
+    this.getAgendas();
+    
   }
 }
 
--- src/app/components/admin/dashboard/calendar/calendar.module.ts
+++ src/app/components/admin/dashboard/calendar/calendar.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
+registerLocaleData(localeEs);
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    NgModule,
   ]
 })
 export class CalendarModule { }
