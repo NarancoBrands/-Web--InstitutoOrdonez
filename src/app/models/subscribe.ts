be2580dffc09c51dc63649bc29e131e0ae583dd1
IvanGarciaRubio <ivan.garcia@narancobrands.com>
2021-06-07 16:00:47 +0200

    sadsds

--- src/app/models/subscribe.ts
+++ src/app/models/subscribe.ts
@@ -1,4 +1,5 @@
 export class Subscribe{
-    constructor(public id:number,public topic:string,public mensaje:string){
+    constructor(public id:number,public topic:string,public code:string, public user_id:string,public temperature:string,
+        public mask:string){
     }
 }
